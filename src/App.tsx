4c64777082aaed1871b1f5bc56bdf31a73bbf096
Macarez Anthony <la238361@student.helha.be>
2025-08-08 03:51:04 +0200

    Nettoyage du code pour la production

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ const App = () => (
             <Route path="/partenaires" element={<Partners />} />
             <Route path="/infos" element={<Info />} />
             <Route path="/contact" element={<Contact />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
